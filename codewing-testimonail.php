a456d5fd866cb79063d3ee1eab265b5021378987
Govinda <dahalgovinda2014@gmail.com>
2023-04-05 10:34:08 +0545

    initial commit

---
+++ codewing-testimonail.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: codewing Test Testimonail
+Plugin URI: https://wordpress.org/plugins/codewing-test-testimonal/
+Description: Basic Testimonal Test
+Version: 1.0.0
+Author: Govinda
+Author URI: https://www.govindahal.com.np
+License: GPL3
+License URI: http://www.gnu.org/licenses/gpl.html
+Text Domain: codewing-test-testimonal
+*/
+
